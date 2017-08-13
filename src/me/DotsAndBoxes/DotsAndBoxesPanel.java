19f86bb453247d2c1f54fdfd05dc5491cb6499d8
RhymeLyet <rhymeinparentheses@gmail.com>
2017-08-13 12:49:39 -0700

    Added the dots

--- src/me/DotsAndBoxes/DotsAndBoxesPanel.java
+++ src/me/DotsAndBoxes/DotsAndBoxesPanel.java
@@ -1,12 +1,28 @@
 package me.DotsAndBoxes;
 
 import java.awt.Color;
+import java.awt.Graphics;
 
 import javax.swing.JPanel;
+
+import me.DotsAndBoxes.Grid.Dots;
 
 @SuppressWarnings("serial")
 public class DotsAndBoxesPanel extends JPanel {
 	public DotsAndBoxesPanel() {
 		this.setBackground(Color.DARK_GRAY);
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		for (Dots dot : Dots.dots) {
+			g.setColor(Color.BLACK);
+			g.drawLine(dot.getX() - 1, dot.getY(), dot.getX() + 1, dot.getY());
+			g.drawLine(dot.getX(), dot.getY() - 1, dot.getX(), dot.getY() + 1);
+			g.setColor(new Color(0, 0, 0, 120));
+			g.drawLine(dot.getX() - 2, dot.getY() - 1, dot.getX(), dot.getY() - 1);
+			g.drawLine(dot.getX() + 2, dot.getY() - 1, dot.getX() + 2, dot.getY() - 1);
+		}
+
 	}
 }
--- src/me/DotsAndBoxes/Grid/Dots.java
+++ src/me/DotsAndBoxes/Grid/Dots.java
@@ -1,5 +1,38 @@
 package me.DotsAndBoxes.Grid;
 
-public class Dots {
+import java.util.ArrayList;
 
+public class Dots {
+	public static ArrayList<Dots> dots = new ArrayList<Dots>();
+	private int x;
+	private int y;
+
+	public Dots(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public static void loadDots(Grid grid) {
+		for (int i = 0; i < grid.getHorizontalRows(); i++) {
+			for (int j = 0; j < grid.getVerticalRows(); j++) {
+				dots.add(new Dots((i * grid.getSpace()) + grid.getSpace(), (j * grid.getSpace()) + grid.getSpace()));
+			}
+		}
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
 }
